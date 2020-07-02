cb9ca27100168031c9d32f9847ae2c39a0d0087e
xsy <303775564@qq.com>
2020-07-02 14:33:16 +0800

    提交架构

--- app/src/main/java/convert/myapp/com/myapplication/avtivity/MainActivity.java
+++ app/src/main/java/convert/myapp/com/myapplication/avtivity/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends BaseActivity {
         super.initView();
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //
 
 
     }
